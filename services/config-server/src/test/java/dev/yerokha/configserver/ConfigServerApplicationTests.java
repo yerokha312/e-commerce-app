66ee509575bb93a794d3cf1e83c3ae221ad27ca4
yerbolat <erbolatt@live.com>
2024-06-16 20:54:54 +0500

    imported config-server

---
+++ services/config-server/src/test/java/dev/yerokha/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yerokha.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
